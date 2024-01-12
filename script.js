094c09aa7a8f4c9a6afa83ef0d55388e3d904c42
Koh0920 <149476241+Koh0920@users.noreply.github.com>
2024-01-12 18:27:30 +0900

    a

--- script.js
+++ script.js
@@ -15,6 +15,16 @@ const options = {
 windyInit(options, windyAPI => {
   const { map, store } = windyAPI;
 
+  // Creating a custom icon
+  const warningIcon = L.icon({
+      iconUrl: './possibility.svg',
+      iconSize: [200, 200], // Size of the icon
+      iconAnchor: [25, 25], // Anchor point of the icon
+  });
+
+  // Adding a marker with the custom icon to the map
+  const marker = L.marker([34.5, 134.1], { icon: warningIcon }).addTo(map);
+
   // Custom control for the icon
   const CustomControl = L.Control.extend({
     onAdd: function() {
@@ -32,25 +42,4 @@ windyInit(options, windyAPI => {
 
 // Adding the custom control to the map
 new CustomControl({ position: 'topleft' }).addTo(map);
-
-  // Creating a custom icon
-  const warningIcon = L.icon({
-      iconUrl: './possibility.svg',
-      iconSize: [50, 50], // Size of the icon
-      iconAnchor: [25, 25], // Anchor point of the icon
-  });
-
-  // Adding a marker with the custom icon to the map
-  const marker = L.marker([34.5, 134.1], { icon: warningIcon }).addTo(map);
-
-  const levels = store.getAllowed('availLevels');
-  let i = 0;
-  setInterval(() => {
-      i = i === levels.length - 1 ? 0 : i + 1;
-      store.set('level', levels[i]);
-  }, 500);
-
-  store.on('level', level => {
-      console.log(`Level was changed: ${level}`);
-  });
 });
